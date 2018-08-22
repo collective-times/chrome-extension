d58433a2e5a961e35c9f53e2f3adc8d51333cb7b
moonstruckdrops <moonstruckdrops@gmail.com>
2018-08-22 19:18:36 +0900

    sample

--- js/popup.js
+++ js/popup.js
@@ -9,6 +9,10 @@ async function getHtmlTextByUrl(url, callback){
 }
 
 function findFeedsByHtmlBody(url, body, callback){
+  if(!url){
+    return;
+  }
+
   if(body === ''){
     callback([]);
   }
@@ -28,6 +32,9 @@ function findFeedsByHtmlBody(url, body, callback){
     'text/rdf'
   ];
 
+  const parser = new URL(url);
+  const contentsUrl = `${parser.protocol}//${parser.host}`;
+
   document.getElementById('rss-feed-url_response').innerHTML = body;
   const links = document.getElementById('rss-feed-url_response').querySelectorAll("#rss-feed-url_response link[type]");
   document.getElementById('rss-feed-url_response').innerHTML = '';
@@ -35,21 +42,22 @@ function findFeedsByHtmlBody(url, body, callback){
   let feeds = [];
   for (var i = 0; i < links.length; i++) {
     if (links[i].hasAttribute('type') && types.indexOf(links[i].getAttribute('type')) !== -1) {
-      var feed_url = links[i].getAttribute('href');
-      if (feed_url.indexOf("//") == 0) {
-        feed_url = "http:" + feed_url;
-      } else if (feed_url.startsWith('/')) {
-        feed_url = url.split('/')[0] + '//' + url.split('/')[2] + feed_url;
-      } else if (/^(http|https):\/\//i.test(feed_url)) {
-        feed_url = feed_url;
+      let feedUrl = links[i].getAttribute('href');
+      if (feedUrl.indexOf("//") == 0) {
+        feedUrl = "http:" + feedUrl;
+      } else if (feedUrl.startsWith('/')) {
+        feedUrl = url.split('/')[0] + '//' + url.split('/')[2] + feedUrl;
+      } else if (/^(http|https):\/\//i.test(feedUrl)) {
+        feedUrl = feedUrl;
       } else {
-        feed_url = url + "/" + feed_url.replace(/^\//g, '');
+        feedUrl = url + "/" + feedUrl.replace(/^\//g, '');
       }
 
       feeds.push({
-        type: links[i].getAttribute('type'),
-        url: feed_url,
-        title: links[i].getAttribute('title') || feed_url
+        contentsUrl: contentsUrl,
+        feedUrl: feedUrl,
+        feedType: links[i].getAttribute('type'),
+        feedTitle: links[i].getAttribute('title') || feedUrl,
       });
     }
   }
@@ -61,23 +69,47 @@ function render(html){
   document.getElementById('feeds').innerHTML = html;
 }
 
+function onClickData(event) {
+  const data = JSON.parse(event.target.dataset.feed);
+
+  let body = {
+    title: data.contentsUrl,
+    feedUrl: data.feedUrl,
+    sourceUrl: data.contentsUrl,
+    crawlable: true,
+    type: "rss"
+  };
+
+
+  render(body);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    var tab = tabs[0];
-    var url = tab.url;
+    let tab = tabs[0];
+    let url = tab.url;
 
-    getHtmlTextByUrl('http://blog.hypermkt.jp', (url, body) => {
+    getHtmlTextByUrl(url, (url, body) => {
       findFeedsByHtmlBody(url, body, (feeds) => {
         if(feeds.length === 0){
           render("No feed found");
           return;
         }
 
-        const feedList = feeds.map((feed) => {
-          return `<li><a href="${feed.url}" title="${feed.type}" target="_blank">${feed.title}</a></li>`;
+        const dataList = feeds.map((feed, index) => {
+          const data = JSON.stringify(feed);
+          return `<li><button id="feed${index}" data-feed=${data}>${feed.feedTitle}</button></li>`;
         });
-        const html = `<ul>${feedList}</ul>`;
+        const feedList = feeds.map((feed) => {
+          return `<li><a href="${feed.feedUrl}" title="${feed.feedType}" target="_blank">${feed.feedTitle}</a></li>`;
+        });
+        const html = `<h2>Candidate</h2><ul>${dataList}</ul><h2>Feed</h2><ul>${feedList}</ul>`;
         render(html);
+
+        feeds.each((index) => {
+          const button = document.getElementById("feed${index}");
+          button.onclick = onClickData;
+        });
       });
     });
   });
